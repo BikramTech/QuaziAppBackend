f62c6b8c6b3ae0cf6badd107c54d240c2b2d11fd
FaramondTech <73873598+FaramondTech@users.noreply.github.com>
2021-10-13 13:26:30 +0530

    skills issue ficex

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -18,7 +18,7 @@ const QzSkills = require('../db/models/keyskills/Qz_Key_Skills')
 const ObjectId = require('mongodb').ObjectID
 
 class UserController {
-  static async userSignup(req, res) {
+  static async userSignup (req, res) {
     try {
       const { user_name, email, mobile_no } = req.body
 
@@ -65,7 +65,7 @@ class UserController {
     }
   }
 
-  static async userLogin(req, res) {
+  static async userLogin (req, res) {
     let user = {}
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     const { email } = req.body
@@ -111,8 +111,8 @@ class UserController {
         !userProfile.status
           ? 'Your account is inactive. Please contact administrator!'
           : !userProfile.is_email_verified
-            ? 'Please verify your email.'
-            : ''
+          ? 'Please verify your email.'
+          : ''
       )
     }
 
@@ -189,7 +189,7 @@ class UserController {
   //   }
   // }
 
-  static async emailVerification(req, res) {
+  static async emailVerification (req, res) {
     const { email, otp } = req.body
 
     try {
@@ -259,7 +259,7 @@ class UserController {
     }
   }
 
-  static async socialLogin(req, res) {
+  static async socialLogin (req, res) {
     try {
       const { email } = req.body
 
@@ -351,7 +351,7 @@ class UserController {
     }
   }
 
-  static async profileUpdate(req, res) {
+  static async profileUpdate (req, res) {
     try {
       const {
         first_name,
@@ -369,39 +369,51 @@ class UserController {
         social_id,
         social_type,
         gender
-      } = req.body;
+      } = req.body
 
-      const existingSkills = await QzSkills.aggregate([{ $match: { name: { $in: skills } } }]);
+      let userSkills = skills
+      if (!Array.isArray(skills)) {
+        userSkills = skills.split(',')
+      }
+      const existingSkills = await QzSkills.aggregate([
+        { $match: { name: { $in: userSkills } } }
+      ])
 
-      const newSkillsToAdd = skills.filter(x => !existingSkills.find(y => x === y.name)).map(x => {
-        return {
-          updateOne: {
-            filter: { name: x },
-            update: { $set: { name: x } },
-            upsert: true,
+      const newSkillsToAdd = userSkills
+        .filter(x => !existingSkills.find(y => x === y.name))
+        .map(x => {
+          return {
+            updateOne: {
+              filter: { name: x },
+              update: { $set: { name: x } },
+              upsert: true
+            }
           }
-        }
-      });
+        })
 
       if (newSkillsToAdd && newSkillsToAdd.length) {
-
-        await QzSkills.bulkWrite(newSkillsToAdd);
+        await QzSkills.bulkWrite(newSkillsToAdd)
       }
-
 
       const userProfile = new QzUserProfile({
         ...req.body,
         user_id: req.params.id
       })
-      await userProfile.validate();
+      await userProfile.validate()
 
-      const profilePicObj = req.files && req.files.profile_pic ? { profile_pic: req.files.profile_pic[0].path } : {};
-      const resumeFileObj = req.files && req.files.resume_file ? { resume_file: req.files.resume_file[0].path } : {};
+      const profilePicObj =
+        req.files && req.files.profile_pic
+          ? { profile_pic: req.files.profile_pic[0].path }
+          : {}
+      const resumeFileObj =
+        req.files && req.files.resume_file
+          ? { resume_file: req.files.resume_file[0].path }
+          : {}
 
       const user = await QzUserProfile.findOneAndUpdate(
         { user_id: req.params.id },
         {
-          skills,
+          skills: userSkills,
           first_name,
           last_name,
           profile_summary,
@@ -441,7 +453,7 @@ class UserController {
     }
   }
 
-  static async details(req, res) {
+  static async details (req, res) {
     try {
       let user = await QzUserRegistration.findById(req.params.id)
       if (!user)
@@ -523,7 +535,7 @@ class UserController {
     }
   }
 
-  static async forgotPassword(req, res) {
+  static async forgotPassword (req, res) {
     try {
       const user = await QzUserRegistration.findOne({ email: req.body.email })
 
@@ -552,7 +564,7 @@ class UserController {
     }
   }
 
-  static async changePassword(req, res) {
+  static async changePassword (req, res) {
     try {
       let password = req.body.newPassword
       password = helpers.GetEncryptedText(password)
@@ -603,7 +615,7 @@ class UserController {
     }
   }
 
-  static async sendOtp(req, res) {
+  static async sendOtp (req, res) {
     try {
       const { email } = req.body
       let OTP = helpers.GenerateSixDigitCode()
@@ -641,7 +653,7 @@ class UserController {
     }
   }
 
-  static async changeStatus(req, res) {
+  static async changeStatus (req, res) {
     try {
       if (req.body.status) {
         return helpers.SendErrorsAsResponse(
@@ -682,7 +694,7 @@ class UserController {
 
   // User Employment Api Starts :-
 
-  static async AddUserEmployment(req, res) {
+  static async AddUserEmployment (req, res) {
     try {
       const { user_id, employer, designation, start_date, end_date } = req.body
 
@@ -708,7 +720,7 @@ class UserController {
     }
   }
 
-  static async GetUserEmploymentsByUserId(req, res) {
+  static async GetUserEmploymentsByUserId (req, res) {
     try {
       const userEmployments = await QzUserEmployment.find({
         user_id: req.params.user_id
@@ -725,7 +737,7 @@ class UserController {
     }
   }
 
-  static async UpdateUserEmployment(req, res) {
+  static async UpdateUserEmployment (req, res) {
     try {
       const { employer, designation, start_date, end_date } = req.body
 
@@ -754,7 +766,7 @@ class UserController {
     }
   }
 
-  static async DeleteUserEmployment(req, res) {
+  static async DeleteUserEmployment (req, res) {
     try {
       const userEmploymentDeletedResult = await QzUserEmployment.findByIdAndDelete(
         req.params.id
@@ -781,7 +793,7 @@ class UserController {
 
   // User Projects Api Starts :-
 
-  static async AddUserProject(req, res) {
+  static async AddUserProject (req, res) {
     try {
       const {
         user_id,
@@ -815,7 +827,7 @@ class UserController {
     }
   }
 
-  static async GetUserProjectsByUserId(req, res) {
+  static async GetUserProjectsByUserId (req, res) {
     try {
       const userProjects = await QzUserProjects.find({
         user_id: req.params.user_id
@@ -840,7 +852,7 @@ class UserController {
     }
   }
 
-  static async UpdateUserProject(req, res) {
+  static async UpdateUserProject (req, res) {
     try {
       const {
         project_title,
@@ -881,7 +893,7 @@ class UserController {
     }
   }
 
-  static async DeleteUserProject(req, res) {
+  static async DeleteUserProject (req, res) {
     try {
       const userProjectDeletedResult = await QzUserProjects.findByIdAndDelete(
         req.params.id
@@ -908,7 +920,7 @@ class UserController {
 
   // User Certification Api Starts :-
 
-  static async AddUserCertification(req, res) {
+  static async AddUserCertification (req, res) {
     try {
       const {
         user_id,
@@ -940,7 +952,7 @@ class UserController {
     }
   }
 
-  static async GetUserCertificationsByUserId(req, res) {
+  static async GetUserCertificationsByUserId (req, res) {
     try {
       const userCertifications = await QzUserCertification.find({
         user_id: req.params.user_id
@@ -965,7 +977,7 @@ class UserController {
     }
   }
 
-  static async UpdateUserCertification(req, res) {
+  static async UpdateUserCertification (req, res) {
     try {
       const {
         certification_name,
@@ -1004,7 +1016,7 @@ class UserController {
     }
   }
 
-  static async DeleteUserCertification(req, res) {
+  static async DeleteUserCertification (req, res) {
     try {
       const userCertificationDeletedResult = await QzUserCertification.findByIdAndDelete(
         req.params.id
@@ -1031,7 +1043,7 @@ class UserController {
 
   // User Applications Api Starts :-
 
-  static async AddUserApplication(req, res) {
+  static async AddUserApplication (req, res) {
     try {
       const { user_id, job_id } = req.body
       const last_update_date = new Date().toISOString()
@@ -1075,7 +1087,7 @@ class UserController {
     }
   }
 
-  static async GetUserApplicationsByUserId(req, res) {
+  static async GetUserApplicationsByUserId (req, res) {
     try {
       const sortBy = req.body.sortBy || 'creation_date'
       const sortOrder = req.body.sortOrder || -1
@@ -1151,7 +1163,7 @@ class UserController {
     }
   }
 
-  static async DeleteUserApplication(req, res) {
+  static async DeleteUserApplication (req, res) {
     try {
       const userApplicationDeletedResult = await QzUserApplications.findByIdAndDelete(
         req.params.id
